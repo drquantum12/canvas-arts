335eb6062abc06f244dc3e017477cdb9ce0fe9e2
Arjun Singh Tomar <drquantum@Arjuns-MacBook-Air.local>
2023-04-15 11:49:39 +0530

    changed background color to green

---
+++ public/configs.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDR0CoHj1jNIlDoxLOxsAVtrsTQJ9wUpxM",
+    authDomain: "canvas-arts-fca86.firebaseapp.com",
+    projectId: "canvas-arts-fca86",
+    storageBucket: "canvas-arts-fca86.appspot.com",
+    messagingSenderId: "899299665656",
+    appId: "1:899299665656:web:8f63b19c5899089d3c7d0d",
+    measurementId: "G-XGS5Y34TDG"
+  };
